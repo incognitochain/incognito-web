e13ba0a991252f0419f5944bf49bb8c433b97b84
Incognito Developer <develop@incognito.org>
2019-05-21 14:29:24 +0700

    Init

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: path.resolve(__dirname, './src/template/index.pug')
+    }),
+  ],
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    },
+    { 
+      test: /\.pug$/,
+      use: ['pug-loader']
+    }]
+  }
+};
+
+const prodConfig = {
+  mode: 'production',
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: path.resolve(__dirname, './src/template/index.pug')
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    },
+    { 
+      test: /\.pug$/,
+      use: ['pug-loader']
+    }]
+  }
+};
+
+  module.exports = (env, argv) => {
+    const isProduction = (argv.mode === 'production');
+
+    return {
+      entry: path.resolve(__dirname, './src/index.js'),
+      devtool: 'inline-source-map',
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+      },
+      ...isProduction ? prodConfig : devConfig
+    };
+  };
