8faeaa445919d460cf319228e1835ac0de0647cd
Keresh <vuong@autonomous.nyc>
2019-05-23 16:38:23 +0700

    Update animation

---
+++ src/visible_effect.js
@@ -0,0 +1,22 @@
+const visible = () => {
+  var intersectionObserver = new IntersectionObserver(function(entries) {
+    // If intersectionRatio is 0, the target is out of view
+    // and we do not need to do anything.
+    const entry = entries[0];
+    const target = entry.target;
+    const classAni = 'fadeInLeftBig-ani';
+
+    if (entry.intersectionRatio <= 0) {
+      return;
+    };
+  
+    if (!target.classList.contains(classAni)) {
+      target.classList.add(classAni);
+    }
+  });
+
+  // start observing
+  document.querySelectorAll('.visible-effect').forEach(item => intersectionObserver.observe(item));
+}
+
+visible();
