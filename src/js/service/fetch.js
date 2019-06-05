82519455d15011a612d0acb0c9469a14880101af
Incognito Developer <develop@incognito.org>
2019-06-05 16:01:50 +0700

    Add fetch

---
+++ src/js/service/fetch.js
@@ -0,0 +1,23 @@
+import { getErrorMessage } from './errorHandler';
+
+const customFetch = (directUrl, { url, method, body, headers }) => {
+  const _url = directUrl || url;
+  return fetch(`${APP_ENV.BASE_API_URL}/${_url}`, {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      ...headers
+    }
+  })
+  .then(response => response.json())
+  .then(json => {
+    if (json && json.Error) {
+      throw new Error(getErrorMessage(json.Error));
+    }
+
+    return json;
+  })
+}
+
+export default customFetch;
