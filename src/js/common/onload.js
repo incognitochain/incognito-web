071519d2e0cf428de39c61b30b51f70391aecb0b
Keresh <vuong@autonomous.nyc>
2019-06-07 16:41:42 +0700

    Add verify email

--- src/js/common/onload.js
+++ src/js/common/onload.js
@@ -1,5 +1,8 @@
 import storage from '../service/storage';
 import KEYS from "../constant/keys";
+import { setMessage } from '../service/message_box';
+import queryString from '../service/queryString';
+import { verifyEmailToken } from '../service/api';
 
 const checkAuth = () => {
   const token = storage.get(KEYS.TOKEN);
@@ -9,8 +12,23 @@ const checkAuth = () => {
   }
 }
 
+const checkVerifyEmailToken = () => {
+  const referralToken = queryString(KEYS.TOKEN_EMAIL_VERIFY_QS);
+  if (referralToken) {
+    verifyEmailToken(referralToken)
+      .then(() => {
+        setMessage('Your email was verified', 'info');
+      })
+      .catch((e) => {
+        setMessage(e.message, 'error');
+      });
+  }
+}
+
 const main = () => {
   checkAuth();
+  checkVerifyEmailToken();
 };
+
 
 main();
---
+++ src/js/service/queryString.js
@@ -0,0 +1,10 @@
+export default key => {
+  let rs;
+  try {
+    rs = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+  } catch (e) {
+    console.error(e);
+  }
+
+  return rs;
+}
