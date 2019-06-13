ea069172e260c25c8d8c7a345a5c04ae9edc06ab
Keresh <vuong@autonomous.nyc>
2019-06-13 18:52:18 +0700

    Update layout

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -1,1 +1,10 @@
 import './intro';
+import bg from '../../image/home_bg.webp';
+
+// change bg
+if (location.pathname === '/')
+  document.body.style.cssText = `
+    background: url('${bg}') white;
+    background-repeat: no-repeat;
+    background-size: cover;
+  `;
