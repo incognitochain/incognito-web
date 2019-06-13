73e63fe28d83deb13e9d759fada4ef0e5123cd6b
Incognito Developer <develop@incognito.org>
2019-06-13 16:03:18 +0700

    Update layout

--- src/js/common/header_mobile.js
+++ src/js/common/header_mobile.js
@@ -2,11 +2,16 @@ const headerEl = document.querySelector('#header .mobile-sidebar');
 const hamburgerBtnEl = headerEl && headerEl.querySelector('.hamburger-btn');
 const sidebarEl = headerEl && headerEl.querySelector('.sidebar');
 const overlayEl = headerEl && headerEl.querySelector('.overlay');
+const closeBtnEl = headerEl && headerEl.querySelector('.close');
 
 hamburgerBtnEl && hamburgerBtnEl.addEventListener('click', () => {
   sidebarEl.classList.add('show');
 });
 
 overlayEl && overlayEl.addEventListener('click', () => {
+  sidebarEl.classList.remove('show');
+});
+
+closeBtnEl && closeBtnEl.addEventListener('click', () => {
   sidebarEl.classList.remove('show');
 });
