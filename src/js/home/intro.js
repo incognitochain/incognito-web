4238f940d3069f2d88c5aeb2e0a87bc5dffba50e
Incognito Developer <develop@incognito.org>
2019-06-25 17:04:36 +0700

    Update home

--- src/js/home/intro.js
+++ src/js/home/intro.js
@@ -45,4 +45,4 @@ const main = () => {
 
 main();
 
-changeCryptoName({ listName: ['Bitcoin', 'Ethereum', 'USDC'], timeout: 4000 });
+changeCryptoName({ listName: ['Bitcoin', 'Ethereum', 'Crypto'], timeout: 4000 });
