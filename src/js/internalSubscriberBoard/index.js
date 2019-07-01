4fd0bed42862d592e0f6707ca4ba9c329688672e
Keresh <vuong@autonomous.nyc>
2019-07-01 13:57:28 +0700

    Add silient

--- src/js/internalSubscriberBoard/index.js
+++ src/js/internalSubscriberBoard/index.js
@@ -4,6 +4,19 @@ import { setMessage } from '../service/message_box';
 const container = document.querySelector('#internal-subscriber-board');
 let currentSubscriberNum = 0;
 
+const isSilient = (now) => {
+  let silient = false;
+  try {
+    const h = now.getHours();
+    const m = now.getMinutes();
+    if (h === 12 || (h === 13 && m < 30)) { // silient from 12PM - 13:30
+      return true;
+    }
+  } catch (e) {}
+
+  return silient;
+}
+
 const getSubscriber = async () => {
   try {
     if (!container) return;
@@ -17,7 +30,10 @@ const getSubscriber = async () => {
       currentSubscriberNum = subscriberNum;
 
       countEl.innerText = Intl.NumberFormat().format(currentSubscriberNum);
-      notiEl.play();
+
+      if (!isSilient(new Date())) {
+        notiEl.play();
+      }
     }
   } catch (e) {
     setMessage(`Get subscriber with error: ${e.message}`, 'error', 10000);
