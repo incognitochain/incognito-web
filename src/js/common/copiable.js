cc88222a27ca0da021b92615ef8e4c3e2322f65d
Keresh <vuong@autonomous.nyc>
2019-07-01 10:12:08 +0700

    Fixed copiable

--- src/js/common/copiable.js
+++ src/js/common/copiable.js
@@ -9,7 +9,34 @@ const copiable = e => {
   const textArea = document.createElement("textarea");
   textArea.value = text;
   document.body.appendChild(textArea);
-  textArea.select();
+
+  // handle iOS as a special case
+  if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+
+    // save current contentEditable/readOnly status
+    var editable = textArea.contentEditable;
+    var readOnly = textArea.readOnly;
+
+    // convert to editable with readonly to stop iOS keyboard opening
+    textArea.contentEditable = true;
+    textArea.readOnly = true;
+
+    // create a selectable range
+    var range = document.createRange();
+    range.selectNodeContents(textArea);
+
+    // select the range
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    textArea.setSelectionRange(0, 999999);
+
+    // restore contentEditable/readOnly to original state
+    textArea.contentEditable = editable;
+    textArea.readOnly = readOnly;
+  } else {
+    textArea.select();
+  }
 
   try {
     var successful = document.execCommand('copy');
