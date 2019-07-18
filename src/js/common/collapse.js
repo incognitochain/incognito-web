f455e32e37e07e226a4e0efcc15d2536abbc3fbf
Incognito Developer <develop@incognito.org>
2019-07-18 18:28:40 +0700

    Update collapse & top question

--- src/js/common/collapse.js
+++ src/js/common/collapse.js
@@ -1,29 +1,44 @@
 import { trackEvent } from './utils/ga';
 
 /**
- * required class: '.collapse'
+ * required class: '.collapse-container'
  */
 
- const collapse = e => {
-  const className = 'show-collapse';
-  if (e.classList.contains(className)) {
-    e.classList.remove(className);
-  } else {
-    e.classList.add(className);
-  }
+const collapseClassName = 'show-collapse';
+
+const show = e => e && e.classList.add(collapseClassName);
+
+const hide = e => e && e.classList.remove(collapseClassName);
+
+const getCollapseGroups = () => document.querySelectorAll('.collapse-container');
+
+const getItems = collapse => collapse && collapse.querySelectorAll('.collapse');
+
+const main = () => {
+  const collapses = getCollapseGroups();
+  collapses && collapses.forEach(collapse => {
+    let currentItem = null;
+    const items = getItems(collapse);
+
+    items && items.forEach(item => {
+      item && item.addEventListener('click', () => {
+        trackEvent({
+          eventCategory: 'Link',
+          eventAction: 'toggle collapse',
+          eventLabel: 'Toggle Collapse'
+        });
+
+        if (currentItem === item) {
+          hide(currentItem);
+          currentItem = null;
+        } else {
+          show(item);
+          hide(currentItem);
+          currentItem = item;
+        }
+      });
+    })
+  });
 };
 
-const els = document.querySelectorAll('.collapse');
-
-els && els.forEach(item => {
-  const handleClick = () => collapse(item);
-  item.addEventListener('click', () => {
-    trackEvent({
-      eventCategory: 'Link',
-      eventAction: 'toggle collapse',
-      eventLabel: 'Toggle FAQs'
-    });
-
-    handleClick();
-  });
-});
+main();
