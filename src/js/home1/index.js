d8f87302c228e0ab4cbe2e0533d8bbd4d2f67762
Incognito Developer <develop@incognito.org>
2019-09-10 16:20:10 +0700

    Update

--- src/js/home1/index.js
+++ src/js/home1/index.js
@@ -16,7 +16,7 @@ function main() {
 
 const startCountdown = (container) => {
   const countdownEl = container.querySelector('.countdown');
-  countdown(countdownEl, '2019-09-24T00:00:00.000', () => {
+  countdown(countdownEl, '2019-09-25T00:00:00.000', () => {
     setMessage('The program was ended', 'error');
   });
 }
