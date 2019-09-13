35f8e2b59e88d1d856561a43674f75bf0fbde830
Keresh <vuong@autonomous.nyc>
2019-09-13 14:11:31 +0700

    Update

--- src/js/common/utils/isPathname.js
+++ src/js/common/utils/isPathname.js
@@ -2,6 +2,7 @@ export default (pathname) => {
   if (typeof window === 'object') {
     let path = window.location.pathname || '';
     path = path.replace('.html', '');
+    pathname = pathname.replace('.html', '');
 
     return path === pathname
   }
