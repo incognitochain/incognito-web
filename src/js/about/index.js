25646d917d2b9b6b33645a9c76f5b470b08c3e4d
0xday <hoai@incognito.org>
2019-09-19 16:00:55 +0700

    update about page

---
+++ src/js/about/index.js
@@ -0,0 +1,53 @@
+const getAllTeamGroups = (container) => {
+    return container.querySelectorAll('.gallery .group');
+}
+
+const getAllTeamGroupButtons = (container) => {
+    return container.querySelectorAll('.team-group .group');
+}
+
+const activeGroup = (container, groupId) => {
+    const teamGroups = getAllTeamGroups(container);
+    const teamGroupButtons = getAllTeamGroupButtons(container);
+
+    teamGroups.forEach((elm) => {
+        const elmId = elm.id;
+        
+        if(elmId === groupId) {
+            elm.classList.add("active");
+            elm.classList.add("visible-effect");
+            elm.classList.add("fadeIn-ani");
+        } else {
+            elm.classList.remove("active");
+        }
+    });
+
+    teamGroupButtons.forEach((elm) => {
+        const elmId = elm.id;
+
+        if(elmId === groupId) {
+            elm.classList.add("active");
+        } else {
+            elm.classList.remove("active");
+        }
+    })
+}
+
+const handleSwitchTeamGroup = (container) => {
+    const teamGroupButtons = getAllTeamGroupButtons(container);
+
+    teamGroupButtons.forEach((item) => {
+        item.addEventListener('click', () => {
+            const elmId = item.getAttribute('filter');
+
+            activeGroup(container, elmId);
+        })
+    })
+}
+
+const main = () => {
+    const container = document.querySelector("#about-container");
+    handleSwitchTeamGroup(container);
+}
+
+main();
--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ import '../scss/main.scss';
 import './referral';
 import './common';
 import './home1';
+import './about';
 // import './internalSubscriberBoard';
