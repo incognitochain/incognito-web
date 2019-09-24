1bb32165c7c09d48cbfafcc8870685744f638b26
0xday <hoai@incognito.org>
2019-09-24 17:02:27 +0700

    update

--- src/js/constant/errorCode.js
+++ src/js/constant/errorCode.js
@@ -2,5 +2,7 @@ export default {
   '-1000': 'Please enter a valid email address',
   '-1005': 'This email address is already in our system. Please try another.',
   '-1019': 'Your email has been verified.',
-  '-1007': 'Looks like the verification link you clicked on was invalid or expired'
+  '-1007': 'The verification link you clicked on was invalid',
+  '-1025': 'You already signed up',
+  '-1026': 'The verification link you clicked on was expired'
 };
