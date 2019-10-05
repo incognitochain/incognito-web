ea7b3ce3a6ebd7bab293528473d4aa18e399edd6
0xday <hoai@incognito.org>
2019-10-05 18:10:43 +0700

    update images

--- src/js/common/scroll_action.js
+++ src/js/common/scroll_action.js
@@ -3,14 +3,15 @@
  * required attr: 'data-scroll-to'
  */
 
- const els = document.querySelectorAll('.scroll-action-btn');
+const els = document.querySelectorAll('.scroll-action-btn');
 
- if (els.length > 0) {
+if (els.length > 0) {
+  console.log('aaa');
   els.forEach(el => {
     el.addEventListener('click', () => {
       const target = el.getAttribute('data-scroll-to');
       const scrollEl = target && document.querySelector(target);
       scrollEl && scrollEl.scrollIntoView({ behavior: 'smooth' });
-    })
-  })
- }
+    });
+  });
+}
