16d6f930a374862b529045a2ab2ea4aa3d20a728
0xday <hoai@incognito.org>
2019-10-18 17:36:54 +0700

    add GA ecommerce tracking

--- src/js/common/utils/ga.js
+++ src/js/common/utils/ga.js
@@ -1,8 +1,87 @@
-export const trackEvent = ({ eventCategory, eventAction, eventLabel, eventValue } = {}) => {
+export const trackEvent = ({
+  eventCategory,
+  eventAction,
+  eventLabel,
+  eventValue
+} = {}) => {
   if (typeof window.ga !== 'function') {
     return;
   }
 
-  console.log('Track event', {eventCategory, eventAction, eventLabel, eventValue});
-  window.ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
-}
+  console.log('Track event', {
+    eventCategory,
+    eventAction,
+    eventLabel,
+    eventValue
+  });
+  window.ga(
+    'send',
+    'event',
+    eventCategory,
+    eventAction,
+    eventLabel,
+    eventValue
+  );
+};
+
+const addProductTrackEvent = ({
+  id,
+  name,
+  category = '',
+  brand = '',
+  variant = '',
+  price = 0,
+  quantity = 1,
+  coupon = '',
+  position = 1
+}) => {
+  if (typeof window.ga !== 'function') return;
+
+  console.log('Add product track event', {
+    id,
+    name,
+    category,
+    brand,
+    variant,
+    price,
+    quantity,
+    coupon,
+    position
+  });
+  window.ga('ec:addProduct', {
+    id,
+    name,
+    category,
+    brand,
+    variant,
+    price,
+    quantity,
+    coupon,
+    position
+  });
+};
+
+const setActionTrackEvent = (action, options) => {
+  if (typeof window.ga !== 'function') return;
+
+  console.log('Set action track event', { action, options });
+  window.ga('ec:setAction', action, { ...options });
+};
+
+export const checkoutTrackEvent = (
+  { product, options },
+  checkoutType = 'checkout'
+) => {
+  addProductTrackEvent({ ...product });
+  setActionTrackEvent(checkoutType, options);
+};
+
+export const addCartTrackEvent = product => {
+  addProductTrackEvent(product);
+  setActionTrackEvent('add');
+};
+
+export const purchaseTrackEvent = ({ product, options }) => {
+  addProductTrackEvent({ ...product });
+  setActionTrackEvent('purchase', options);
+};
