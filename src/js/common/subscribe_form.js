0d4cb2aa7f7b9fa9efa96b15ae21467a87327b34
0xday <hoai@incognito.org>
2019-10-25 14:38:01 +0700

    optimize & remove unused code

--- src/js/common/subscribe_form.js
+++ src/js/common/subscribe_form.js
@@ -5,68 +5,68 @@ import KEYS from '../constant/keys';
 import { trackEvent } from './utils/ga';
 import queryString from '../service/queryString';
 
-const getReferralCode = () => {
-  return storage.get(KEYS.REFERRAL_CODE) || undefined;
-};
+// const getReferralCode = () => {
+//   return storage.get(KEYS.REFERRAL_CODE) || undefined;
+// };
 
-const formHandle = () => {
-  const forms = document.querySelectorAll('form#email-subscribe');
-  for (let form of forms) {
-    const emailEl = form && form.querySelector('#email-input');
-    const submitBtn = form && form.querySelector('button.submit-email');
+// const formHandle = () => {
+//   const forms = document.querySelectorAll('form#email-subscribe');
+//   for (let form of forms) {
+//     const emailEl = form && form.querySelector('#email-input');
+//     const submitBtn = form && form.querySelector('button.submit-email');
 
-    emailEl &&
-      emailEl.addEventListener('input', function(event) {
-        if (emailEl.validity.patternMismatch || emailEl.validity.typeMismatch) {
-          emailEl.setCustomValidity('This is not valid email!');
-        } else {
-          emailEl.setCustomValidity('');
-        }
-      });
+//     emailEl &&
+//       emailEl.addEventListener('input', function(event) {
+//         if (emailEl.validity.patternMismatch || emailEl.validity.typeMismatch) {
+//           emailEl.setCustomValidity('This is not valid email!');
+//         } else {
+//           emailEl.setCustomValidity('');
+//         }
+//       });
 
-    form &&
-      form.addEventListener('submit', async e => {
-        trackEvent({
-          eventCategory: 'Button',
-          eventAction: 'submit',
-          eventLabel: 'Subscribe email'
-        });
+//     form &&
+//       form.addEventListener('submit', async e => {
+//         trackEvent({
+//           eventCategory: 'Button',
+//           eventAction: 'submit',
+//           eventLabel: 'Subscribe email'
+//         });
 
-        e.preventDefault();
+//         e.preventDefault();
 
-        //set submit status
-        const submitBtnText = submitBtn.querySelector('#text') || {};
-        const originalBtnText = submitBtnText.innerText;
-        submitBtnText.innerText = 'Sending...';
-        submitBtn.disabled = 'disabled';
-        submitBtn.classList.add('loading');
+//         //set submit status
+//         const submitBtnText = submitBtn.querySelector('#text') || {};
+//         const originalBtnText = submitBtnText.innerText;
+//         submitBtnText.innerText = 'Sending...';
+//         submitBtn.disabled = 'disabled';
+//         submitBtn.classList.add('loading');
 
-        try {
-          const subscribedData = await subscribe(
-            emailEl.value,
-            getReferralCode()
-          );
-          const authData = await auth(emailEl.value);
+//         try {
+//           const subscribedData = await subscribe(
+//             emailEl.value,
+//             getReferralCode()
+//           );
+//           const authData = await auth(emailEl.value);
 
-          setMessage('Your email has been subscribed!');
+//           setMessage('Your email has been subscribed!');
 
-          // store data to local
-          storage.setMultiple({
-            [KEYS.TOKEN]: authData.token,
-            [KEYS.MY_REFERRAL_CODE]: subscribedData.code
-          });
+//           // store data to local
+//           storage.setMultiple({
+//             [KEYS.TOKEN]: authData.token,
+//             [KEYS.MY_REFERRAL_CODE]: subscribedData.code
+//           });
 
-          location.pathname = '/referral.html';
-        } catch (e) {
-          setMessage(e.message, 'error');
-        } finally {
-          submitBtnText.innerText = originalBtnText;
-          submitBtn.disabled = undefined;
-          submitBtn.classList.remove('loading');
-        }
-      });
-  }
-};
+//           location.pathname = '/referral.html';
+//         } catch (e) {
+//           setMessage(e.message, 'error');
+//         } finally {
+//           submitBtnText.innerText = originalBtnText;
+//           submitBtn.disabled = undefined;
+//           submitBtn.classList.remove('loading');
+//         }
+//       });
+//   }
+// };
 
 const handleAutoSignIn = () => {
   const email = queryString('email');
