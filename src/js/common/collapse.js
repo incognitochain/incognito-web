c71ba491cbcde050b4b808f4c2fd0aeda0c2e837
0xday <hoai@incognito.org>
2019-11-01 15:23:58 +0700

    [WIP] update content

--- src/js/common/collapse.js
+++ src/js/common/collapse.js
@@ -10,44 +10,50 @@ const show = e => e && e.classList.add(collapseClassName);
 
 const hide = e => e && e.classList.remove(collapseClassName);
 
-const getCollapseGroups = () => document.querySelectorAll('.collapse-container');
+const getCollapseGroups = () =>
+  document.querySelectorAll('.collapse-container');
 
 const getItems = collapse => collapse && collapse.querySelectorAll('.collapse');
 
 const main = () => {
   const collapses = getCollapseGroups();
-  collapses && collapses.forEach(collapse => {
-    const autoCollapseFirstItem = collapse.getAttribute('auto-collapse-first-item') || '';
-    let currentItem = null;
-    const items = getItems(collapse);
+  collapses &&
+    collapses.forEach(collapse => {
+      const autoCollapseFirstItem =
+        collapse.getAttribute('auto-collapse-first-item') || '';
+      let currentItem = null;
+      const items = getItems(collapse);
 
-    const toggle = (item) => {
-      if (currentItem === item) {
-        hide(currentItem);
-        currentItem = null;
-      } else {
-        show(item);
-        hide(currentItem);
-        currentItem = item;
-      }
-    };
+      const toggle = item => {
+        if (currentItem === item) {
+          hide(currentItem);
+          currentItem = null;
+        } else {
+          show(item);
+          hide(currentItem);
+          currentItem = item;
+        }
+      };
 
-    items && items.forEach((item, index) => {
-      if (autoCollapseFirstItem.toLowerCase() !== 'false' && index === 0) toggle(item);
+      items &&
+        items.forEach((item, index) => {
+          if (autoCollapseFirstItem.toLowerCase() === 'true' && index === 0)
+            toggle(item);
 
-      const label = item.querySelector('.label');
+          const label = item.querySelector('.label');
 
-      label && label.addEventListener('click', () => {
-        // trackEvent({
-        //   eventCategory: 'Link',
-        //   eventAction: 'toggle collapse',
-        //   eventLabel: 'Toggle Collapse'
-        // });
+          label &&
+            label.addEventListener('click', () => {
+              // trackEvent({
+              //   eventCategory: 'Link',
+              //   eventAction: 'toggle collapse',
+              //   eventLabel: 'Toggle Collapse'
+              // });
 
-        toggle(item);
-      });
-    })
-  });
+              toggle(item);
+            });
+        });
+    });
 };
 
 main();
